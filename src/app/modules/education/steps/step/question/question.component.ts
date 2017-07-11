a665385fd01b509f5030091f9dcce029c490ec06
TRANSASIA\demyanyuk.iv <demyanyuk.iv@transasia.ru>
2017-07-11 12:34:16 +0300

    Showed answered question on prev. steps

--- src/app/modules/education/steps/step/question/question.component.ts
+++ src/app/modules/education/steps/step/question/question.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 export class QuestionComponent implements OnInit, OnChanges {
   @Input() question: any;
   @Input() step: any;
+  @Input() steps: Array<any>;
   @Input() answeredQuestions: any;
+
   answer: any = new Object();
+  prevStepsId: Array<number>;
+
   constructor() { }
 
   ngOnInit() {
@@ -17,13 +21,28 @@ export class QuestionComponent implements OnInit, OnChanges {
 
   ngOnChanges() {
     console.log(this.answeredQuestions);
-    
+    this.prevStepsId = this.setPrevSteps(this.steps);
   }
 
   getAnswer(answer) {
     console.log(answer);
     this.answer = answer;
     this.answeredQuestions[this.step.id][this.question.id] = answer;
+  }
+
+  setPrevSteps(steps) {
+    let stepsIds = [];
+    steps.map(step => stepsIds.push(step.id));
+    console.log(stepsIds);
+    let index: number = stepsIds.indexOf(this.step.id);
+    if (index > 0) {
+      console.log(stepsIds.indexOf(this.step.id))
+      stepsIds = stepsIds.slice(0, stepsIds.indexOf(this.step.id));
+    } else {
+      stepsIds = [];
+    }
+
+    return stepsIds;
   }
 
 }
